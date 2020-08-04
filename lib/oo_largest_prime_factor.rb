93db7bb16ef4ab81f9a905366873e7185fbd5eb8
dianakw8591 <dianakw@gmail.com>
2020-08-04 14:54:17 -0700

    Done

--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,30 @@
 # Enter your object-oriented solution here!
+class LargestPrimeFactor
+  attr_reader :n
+
+  def initialize(n)
+    @n = n
+  end
+
+
+  def is_prime(m)
+    i = 2
+    while i <= m/2 do
+      if m % i == 0
+        return false
+      end
+      i += 1
+    end
+    return true
+  end
+  
+  def number
+    i = 1
+    while i <= n/2 do
+      if n % i == 0 && is_prime(n/i)
+        return n/i
+      end
+      i += 1
+    end
+  end
+end
